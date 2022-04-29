e18fcca562594d81b65a9ea448a4a6b25745bd45
Leon <cohenleonlbc@gmail.com>
2022-04-29 16:39:56 -0300

    Ex4 - Ok

--- src/exercicio4/Main.java
+++ src/exercicio4/Main.java
@@ -5,14 +5,14 @@ public class Main {
 		
 		Lista lista = new Lista();
 		
-		// inserção dos elementos na lista
+		// insercao dos elementos na lista
 		lista.inserir(10);
 		lista.inserir(25);
 		lista.inserir(15);
 		lista.inserir(-3);
 		lista.inserir(0);
 		
-		// impressão dos elementos da lista
+		// impressao dos elementos da lista
 		lista.imprimir();
 
 	}
